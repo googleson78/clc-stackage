94ac07db34e835d0bd189d0e234db130c69d00de
Georgi Lyubenov <godzbanebane@gmail.com>
2023-04-05 11:13:51 +0300

    bla

--- flake.nix
+++ flake.nix
@@ -6,24 +6,21 @@
       url = "github:edolstra/flake-compat";
       flake = false;
     };
-    nixpkgs-old.url = "github:nixos/nixpkgs/17b62c338f2a0862a58bb6951556beecd98ccda9";
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   };
 
-  outputs = { self, nixpkgs-old, nixpkgs, ... }:
+  outputs = { self, nixpkgs, ... }:
     let
       system = "x86_64-linux";
-      pkgs-old = import nixpkgs-old {
-        inherit system;
-      };
       pkgs = import nixpkgs {
         inherit system;
       };
-    in with pkgs-old;
+    in with pkgs;
     {
-      devShell.x86_64-linux = pkgs-old.mkShell {
+      devShell.x86_64-linux = mkShell {
         buildInputs = [
-          pkgs.cabal-install
+          haskell.compiler.ghc94
+          cabal-install
           libffi.dev
           libGL
           xorg.libX11
